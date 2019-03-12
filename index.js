c254455dea3362e549c98bbdb576c3c2b85f247d
americanman <snowboardracer7@gmail.com>
2019-03-12 16:09:11 -0400

    Add second solution with split and join methods

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-function titleCase(str) {
+// Original solution
+//const titleCase = (str) => {
+//
+//  str = str.toLowerCase();
+//
+//  let titleStr ='';
+//
+//  for(let i = 0; str.length > i; i++) {
+//    if(str[i] === ' '){
+//      let capital = " " + str[i + 1].toUpperCase()
+//      titleStr += capital;
+//      i++;
+//    }
+//    else {
+//      titleStr += (str[i])
+//    }
+//  }
+//  
+//  return str[0].toUpperCase() + titleStr.slice(1);
+//}
+//
+//console.log(titleCase("I'm a little tea pot"))
 
-  str = str.toLowerCase();
 
-  let titleStr ='';
 
-  for(let i = 0; str.length > i; i++) {
-    if(str[i] === ' '){
-      let capital = " " + str[i + 1].toUpperCase()
-      titleStr += capital;
-      i++;
-    }
-    else {
-      titleStr += (str[i])
-    }
+// solution with split and join methods
+const titleCase = (str) => {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
   }
-  
-  return str[0].toUpperCase() + titleStr.slice(1);
+  return str.join(' ');
 }
+titleCase("I'm a little tea pot");
 
 console.log(titleCase("I'm a little tea pot"))
